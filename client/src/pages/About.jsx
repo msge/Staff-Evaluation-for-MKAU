aaa99ac42bc110899cfa3113c1f7afcbe4633816
Msge Desta <56590675+msge@users.noreply.github.com>
2024-02-11 07:44:17 +0300

    Initial commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About MKAU Staff Evaluation System
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            Welcome to the Staff Evaluation System by Eagle Developer Team! Our user-friendly platform 
+            streamlines evaluations, tracks progress, and generates insightful reports. Set goals, provide
+             feedback, and conduct comprehensive assessments. Elevate your performance management practices
+              and drive success. Thank you for choosing us!
+
+
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
